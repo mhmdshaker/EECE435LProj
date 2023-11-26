df451ceff31ec0e424346f99e697abc85cce5b7e
mhmdshaker <shaker.mohamad01@gmail.com>
2023-11-27 01:45:35 +0200

    i created create_customer function (query)

--- customers.py
+++ customers.py
@@ -16,5 +16,42 @@ if __name__ == '__main__':
 
 
 
+#test
+@app.route('/khalil', methods=['GET'])
+def hello_world():
+    return 'Hello, World!'
 
+# Define the Customer model
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Fullname = db.Column(db.String(255), nullable=False)
+    Username = db.Column(db.String(255), unique=True, nullable=False)
+    Password = db.Column(db.String(255), nullable=False)
+    Age = db.Column(db.Integer)
+    Address = db.Column(db.String(255))
+    Gender = db.Column(db.Enum('Male', 'Female'))
+    MaritalStatus = db.Column(db.Enum('Single', 'Married', 'Divorced', 'Widowed'))
+
+#add customer to DB:
+@app.route('/create_customer', methods=['POST'])
+def create_customer():
+    data = request.get_json()
+    try:
+        new_customer = Customer(
+            Fullname=data['Fullname'],
+            Username=data['Username'],
+            Password=data['Password'],
+            Age=data['Age'],
+            Address=data['Address'],
+            Gender=data['Gender'],
+            MaritalStatus=data['MaritalStatus']
+        )
+
+        db.session.add(new_customer)
+        db.session.commit()
+
+        return jsonify({"message": "Customer created successfully"}), 201
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
