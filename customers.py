65f8eba0d0b9f121624a354948c0acea4ad73698
mhmdshaker <shaker.mohamad01@gmail.com>
2023-11-28 03:57:44 +0200

    fixed an error in the deduct money function

--- customers.py
+++ customers.py
@@ -178,7 +178,7 @@ def deduct_money():
         if type(data['Amount_to_deduct'])!=float:
             return jsonify({"error": "Invalid type for amount to deduct! You shopuld input a decimal or an integer"}), 400
     if(data['Amount_to_deduct'] > customer.wallet):
-        return jsonify({"error: Deduction failed. Wallet does not contain this amount!"})
+        return jsonify({"error": "Deduction failed. Wallet does not contain this amount!"})
 
     customer.wallet -= Decimal(data['Amount_to_deduct'])
     
